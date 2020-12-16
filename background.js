c360432f7690da607b91d25dba66140ae16ecc10
actionless <actionless.loveless@gmail.com>
2020-12-16 04:39:05 +0100

    wip: use new pin-builder url

--- background.js
+++ background.js
@@ -2,14 +2,15 @@
 (function () {
     'use strict';
 
-    const debug = false;
+    //const debug = false;
+    const debug = true;
 
     /*global chrome:false */
     var browser = browser || null;
 
     const pinterestProto = "https://";
     const defaultCountryPrefix = "www";
-    const pinterestURLTemplate = ".pinterest.com/pin/find/?url=";
+    const pinterestURLTemplate = ".pinterest.com/pin-builder/";
     var baseURL = pinterestProto + defaultCountryPrefix + pinterestURLTemplate;
     var pinterestTabsIDs = {};
     var countryChosen = false;
@@ -22,21 +23,22 @@
         console.error(`Error: ${error}`);
     }
 
-    function handlePinterestTabRemoved(tabId, removeInfo) {
+    function handlePinterestCountryCheckRemoved(tabId, removeInfo) {
         if (pinterestTabsIDs[tabId]) {
             delete pinterestTabsIDs[tabId];
         }
     }
 
-    function handlePinterestTabUpdated(tabId, changeInfo, tabInfo) {
+    function handlePinterestCountryCheckUpdated(tabId, changeInfo, tabInfo) {
 
-        function markCountryChosen(countryPrefix) {
+        function markCountryChosen(countryPrefix, searchURL) {
             countryChosen = countryPrefix;
             delete pinterestTabsIDs[tabId];
             if (Object.keys(pinterestTabsIDs).length === 0) {
                 debugLog('REMOVE LISTENER');
-                chrome.tabs.onUpdated.removeListener(handlePinterestTabUpdated);
-                chrome.tabs.onRemoved.removeListener(handlePinterestTabRemoved);
+                chrome.tabs.onUpdated.removeListener(handlePinterestCountryCheckUpdated);
+                chrome.tabs.onRemoved.removeListener(handlePinterestCountryCheckRemoved);
+                doSearch(tabId, searchURL);
             }
         }
 
@@ -47,49 +49,54 @@
                     if (countryPrefix !== defaultCountryPrefix) {
                         baseURL = pinterestProto + countryPrefix + pinterestURLTemplate;
                         chrome.tabs.update(tabId, {
-                            'url': pinterestProto + countryPrefix + '.' + pinterestTabsIDs[tabId]
+                            'url': pinterestProto + countryPrefix + '.' + pinterestTabsIDs[tabId].pinterestURL
                         });
                         console.log(`[pinterest_light:${tabId}] Changing country to "${countryPrefix}"...`);
-                        markCountryChosen(countryPrefix);
+                        markCountryChosen(countryPrefix, pinterestTabsIDs[tabId].searchURL);
                     }
                 } else if (changeInfo.status === 'complete') {
                     debugLog(tabInfo);
                     debugLog("[pinterest_light] Country change doesn't needed.");
-                    markCountryChosen(defaultCountryPrefix);
+                    markCountryChosen(defaultCountryPrefix, pinterestTabsIDs[tabId].searchURL);
                 }
             } else {
-                let countryPrefix = new URL(tabInfo.url).hostname.split('.')[0];
+                debugLog(tabInfo.url);
+                let countryPrefix = new URL(pinterestProto + pinterestTabsIDs[tabId].pinterestURL).hostname.split('.')[0];
+                debugLog([countryPrefix, countryChosen]);
                 if (countryPrefix !== countryChosen) {
                     debugLog(`[pinterest_light:${tabId}] Changing country to ALREADY SELECTED "${countryChosen}"...`);
                     chrome.tabs.update(tabId, {
-                        'url': pinterestProto + countryChosen + '.' + pinterestTabsIDs[tabId]
+                        'url': pinterestProto + countryChosen + '.' + pinterestTabsIDs[tabId].pinterestURL
                     });
-                    markCountryChosen(countryChosen);
+                } else {
+                    debugLog(`[pinterest_light:${tabId}] Country is already selected: "${countryChosen}"...`);
                 }
+                markCountryChosen(countryChosen, pinterestTabsIDs[tabId].searchURL);
             }
         }
     }
 
-    function pinterestTabCreated(tab) {
-        console.log(`Going to pin ${tab.title}`);
-        debugLog(tab);
-        pinterestTabsIDs[tab.id] = tab.title;
-        if (!chrome.tabs.onUpdated.hasListener(handlePinterestTabUpdated)) {
+    function checkPinterestCountry(tab, searchURL) {
+        debugLog('Check language:');
+        pinterestTabsIDs[tab.id] = {'pinterestURL': tab.title, 'searchURL': searchURL};
+        if (!chrome.tabs.onUpdated.hasListener(handlePinterestCountryCheckUpdated)) {
             debugLog('ADD LISTENER');
-            chrome.tabs.onUpdated.addListener(handlePinterestTabUpdated);
-            chrome.tabs.onRemoved.addListener(handlePinterestTabRemoved);
+            chrome.tabs.onUpdated.addListener(handlePinterestCountryCheckUpdated);
+            chrome.tabs.onRemoved.addListener(handlePinterestCountryCheckRemoved);
         }
+    }
+
+    function doSearch(tabId, queryURL) {
+        debugLog(`Do search for ${queryURL}: ${tabId}`);
     }
 
     function foundActiveTabs(tabs) {
         let tab = tabs[0]; // Safe to assume there will only be one result
-        let encodedURL = encodeURIComponent(tab.url);
-        let resultURL = baseURL + encodedURL;
-        let newTabCallback;
-        if (!countryChosen) {
-            newTabCallback = pinterestTabCreated;
+        debugLog(`Found active tabs: ${tab.url}`);
+        let newTabCallback = (newTab) => {
+            checkPinterestCountry(newTab, tab.url);
         }
-        chrome.tabs.create({'url': resultURL, 'active': true}, newTabCallback);
+        chrome.tabs.create({'url': baseURL, 'active': true}, newTabCallback);
     }
 
     function handleNewURL(url) {
